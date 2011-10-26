49aab494aba8739b7490e4d2afa3134acdb9bdd1
Sam Kottler <sam@kottlerdevelopment.com>
2011-10-26 16:15:34 -0400

    Made the action script look more like PHP than C

--- scrubber/bin/action.php
+++ scrubber/bin/action.php
@@ -3,52 +3,41 @@
 // the single script that all ajax and upload requests are made to
 // info about the request is made via POST['action']
 
-$HOME = ".";
-
-require_once( "includes/nav.php" );
-
-require_once( $MODMSG );
-//require_once( $MODUPLOAD );
+require_once("includes/nav.php");
+require_once($MODMSG);
 
 $MSG = new Msg();
-
 $action = $_POST['action'];
-
-if ( !$action )
-{
+if (!$action) {
     $MSG->mlog( 'e', "No action." );
-}
+} 
 else
 {
-    switch ( $action )
-    {
-    case 'uploadtext' :
-        if ( $_FILES['file'] )
-        {
+    switch ($action) {
+    case 'uploadtext':
+        if ($_FILES['file']) {
             $FH = @fopen( $_FILES['file']['tmp_name'], 'r' );
             $str = utf8_encode( 
-                @fread( $FH, filesize( $_FILES['file']['tmp_name'] ) ) 
+                @fread($FH, filesize( $_FILES['file']['tmp_name'])) 
             );
-            @fclose( $FH );
-            if ( $str )
-            {
-                $MSG->rlog( 'textname', $_FILES['file']['name'] );
-                $MSG->rlog( 'text', utf8_decode( $str ) );
+            @fclose($FH );
+            if ($str) {
+                $MSG->rlog('textname', $_FILES['file']['name']);
+                $MSG->rlog('text', utf8_decode($str));
             }
             else
-                $MSG->mlog( 'e', "File upload error." );
+                $MSG->mlog('e', "File upload error.");
         }
         else
-            $MSG->mlog( 'e', "Major file upload error." );
+            $MSG->mlog('e', "Major file upload error.");
         break;
-
     default :
-        $MSG->mlog( 'w', "No action in switch." );
+        $MSG->mlog('w', "No action in switch.");
     }
 }
 
 // output
-header( "Content-type: application/json" );
+header("Content-type: application/json");
 $MSG->json_output();
 
 ?>
