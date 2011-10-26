93972a0ba754193b5da6d24c42d28a50d170a7cd
Sam Kottler <sam@kottlerdevelopment.com>
2011-10-26 16:31:13 -0400

    Added the correct file locations to the index

--- scrubber/bin/index.php
+++ scrubber/bin/index.php
@@ -4,13 +4,13 @@
 <link rel="stylesheet" type="text/css" href="ext/resources/css/ext-all.css" /> 
 <script type="text/javascript" src="ext/adapter/ext/ext-base.js"></script> 
 <script type="text/javascript" src="ext/ext-all-debug.js"></script> 
-<script type="text/javascript" src="scrubber.js"></script> 
+<script type="text/javascript" src="js/scrubber.js"></script> 
 
-<script type="text/javascript" src="./ext/ux/fileuploadfield/FileUploadField.js"></script>
-<link rel="stylesheet" type="text/css" href="./ext/ux/fileuploadfield/css/fileuploadfield.css">
+<script type="text/javascript" src="ext/ux/fileuploadfield/FileUploadField.js"></script>
+<link rel="stylesheet" type="text/css" href="ext/ux/fileuploadfield/css/fileuploadfield.css">
 
 <link rel="stylesheet" type="text/css" href="ext/examples/shared/examples.css" /> 
-<link rel="stylesheet" type="text/css" href="scrubber.css" /> 
+<link rel="stylesheet" type="text/css" href="css/scrubber.css" /> 
 
     <style type="text/css"> 
     .x-panel-body p {
