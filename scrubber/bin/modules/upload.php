d22817472752c77e2eaf407a23f6ea67ff14beba
Sam Kottler <sam@kottlerdevelopment.com>
2011-10-31 16:31:01 -0400

    Refactored the upload script

--- scrubber/bin/modules/upload.php
+++ scrubber/bin/modules/upload.php
@@ -2,25 +2,20 @@
 // /modules/upload.php
 // Upload module for handling all file uploads from texts, lemma lists ...
 
-class Upload
-{
-    function __construct( $files )
-    {
+class Upload {
+    function __construct($files) {
         $this->file = $files;
     }
 
-    public function filetext( $file = 0 )
-    {
-        
+    public function filetext($file = 0) {
+
     }
 
-    private function _readfile( $file = 0 )
-    {
-        //$FH = fopen( 
+    private function readfile($file = 0) {
+        //$FH = fopen(
     }
 
     // data
-
     private $files = null;
 }
 
