c78e991afa52fffa0ddb091f1215af52272d9ec6
Sam Kottler <sam@kottlerdevelopment.com>
2012-01-04 08:19:12 -0500

    Move scrubbing into scrubber.js for new UI

--- scrubber/bin/js/scrubber.js
+++ scrubber/bin/js/scrubber.js
@@ -1,3 +1,105 @@
+Ext.onReady(function(){
+    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+    
+    var viewport = new Ext.Viewport({
+        layout: 'border',
+        items: [
+        // create instance immediately
+        /*new Ext.BoxComponent({
+            region: 'north',
+            height: 32, // give north and south regions a height
+            autoEl: {
+                tag: 'div',
+                html:'<p>north - generally for menus, toolbars and/or advertisements</p>'
+            }
+        }), */{
+            region: 'east',
+            title: 'East Side',
+            collapsible: true,
+            split: true,
+            width: 225, // give east and west regions a width
+            minSize: 175,
+            maxSize: 400,
+            margins: '0 5 0 0',
+            layout: 'fit', // specify layout manager for items
+            items:            // this TabPanel is wrapped by another Panel so the title will be applied
+            new Ext.TabPanel({
+                border: false, // already wrapped so don't add another border
+                activeTab: 1, // second tab initially active
+                tabPosition: 'bottom',
+                items: [{
+                    html: '<p>A TabPanel component can be a region.</p>',
+                    title: 'A Tab',
+                    autoScroll: true
+                }, new Ext.grid.PropertyGrid({
+                    title: 'Property Grid',
+                    closable: true,
+                    source: {
+                        "(name)": "Properties Grid",
+                        "grouping": false,
+                        "autoFitColumns": true,
+                        "productionQuality": false,
+                        "created": new Date(Date.parse('10/15/2006')),
+                        "tested": false,
+                        "version": 0.01,
+                        "borderWidth": 1
+                    }
+                })]
+            })
+        }, {
+            region: 'west',
+            id: 'west-panel', // see Ext.getCmp() below
+            title: 'West',
+            split: true,
+            width: 200,
+            minSize: 175,
+            maxSize: 400,
+            collapsible: true,
+            margins: '0 0 0 5',
+            layout: {
+                type: 'accordion',
+                animate: true
+            },
+            items: [{
+                contentEl: 'west',
+                title: 'Navigation',
+                border: false,
+                iconCls: 'nav' // see the HEAD section for style used
+            }, {
+                title: 'Settings',
+                html: '<p>Some settings in here.</p>',
+                border: false,
+                iconCls: 'settings'
+            }]
+        },
+        // in this instance the TabPanel is not wrapped by another panel
+        // since no title is needed, this Panel is added directly
+        // as a Container
+        new Ext.TabPanel({
+            region: 'center', // a center region is ALWAYS required for border layout
+            deferredRender: false,
+            activeTab: 0,     // first tab initially active
+            items: [{
+                contentEl: 'center1',
+                title: 'Close Me',
+                closable: true,
+                autoScroll: true
+            }, {
+                contentEl: 'center2',
+                title: 'Center Panel',
+                autoScroll: true
+            }]
+        })]
+    });
+    // get a reference to the HTML element with id "hideit" and add a click listener to it 
+    Ext.get("hideit").on('click', function(){
+        // get a reference to the Panel that was created with id = 'west-panel' 
+        var w = Ext.getCmp('west-panel');
+        // expand or collapse that Panel based on its collapsed property state
+        w.collapsed ? w.expand() : w.collapse();
+    });
+});
+/**
 // scrubber.js
 // defines a Scrubber tool and all necessary components needed
 // to make the tool work
@@ -764,5 +866,5 @@ Uploader.Form = Ext.extend( Ext.form.FormPanel, {
         Uploader.Form.superclass.initComponent.apply( this, arguments );
     }
 });
-
+**/
 
